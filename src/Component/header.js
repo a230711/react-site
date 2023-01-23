331c007753bf8cce31d8aa8ec073963b841d1ba6
Leo mini <a230711@yahoo.com.tw>
2023-01-23 14:42:31 +0800

    test2

---
+++ src/Component/header.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+export default class header extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+            <div>
+                <p>jkljl</p>
+            </div>
+        )
+    }
+}
